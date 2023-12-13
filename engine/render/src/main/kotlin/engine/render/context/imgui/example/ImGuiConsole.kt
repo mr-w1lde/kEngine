20acc6a1ca970206da2b365dd8887797ce5c91c3
Stanislav Migunov <starkthedragon@outlook.com>
2023-12-13 22:49:10 +0100

    Add Simple Console

---
+++ engine/render/src/main/kotlin/engine/render/context/imgui/example/ImGuiConsole.kt
@@ -0,0 +1,40 @@
+package engine.render.context.imgui.example
+
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
+import imgui.ImGui
+import imgui.flag.ImGuiInputTextFlags
+import imgui.flag.ImGuiWindowFlags
+import imgui.type.ImString
+import org.slf4j.LoggerFactory
+
+class ImGuiConsole {
+    private val consoleBuffer = StringBuilder()
+
+    private val loggerContext = LoggerFactory.getILoggerFactory() as LoggerContext
+
+    init {
+        val logbackLogger = loggerContext.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+        logbackLogger.addAppender(object : AppenderBase<ILoggingEvent>() {
+            override fun append(event: ILoggingEvent) {
+                val log = "${event.level}: ${event.loggerName} - ${event.formattedMessage}"
+
+                consoleBuffer.appendLine(log)
+            }
+        }.also { it.start() })
+    }
+    fun render() {
+        ImGui.begin("Console", ImGuiWindowFlags.HorizontalScrollbar)
+
+        ImGui.textWrapped(consoleBuffer.toString())
+
+
+        val inputFlags = ImGuiInputTextFlags.None or ImGuiInputTextFlags.EnterReturnsTrue
+        if (ImGui.inputText("##input", ImString(""), inputFlags)) {
+            //val inputText = ImGui.getIO().
+        }
+
+        ImGui.end()
+    }
+}
--- engine/render/src/main/kotlin/engine/render/context/imgui/layer/ImGuiLayer.kt
+++ engine/render/src/main/kotlin/engine/render/context/imgui/layer/ImGuiLayer.kt
@@ -17,8 +17,6 @@ class ImGuiLayer(
     private val windowRef: Window,
     private var mTime: Float = 0.0f,
 ) : Layer("ImGuiLayer") {
-  // private val imguiGlfw = ImGuiImplGlfw()
-
   private val imguiGl3 = ImGuiImplGl3()
 
   override fun onAttach() {
@@ -33,15 +31,11 @@ class ImGuiLayer(
     }
 
     ImGui.init()
-
-    // imguiGlfw.init(windowRef.id, true)
     imguiGl3.init(GLSL_VERSION)
   }
 
   override fun onDetach() {
     imguiGl3.dispose()
-    // imguiGlfw.dispose()
-
     ImGui.destroyContext()
   }
 
@@ -53,11 +47,8 @@ class ImGuiLayer(
     ImGui.getIO().deltaTime = if (mTime > 0.0f) (time - mTime) else (1.0f / 60.0f)
     mTime = time
 
-    // imguiGlfw.newFrame()
     ImGui.newFrame()
-
     ImGui.showDemoWindow(ImBoolean(true))
-
     ImGui.render()
     imguiGl3.renderDrawData(ImGui.getDrawData())
 
